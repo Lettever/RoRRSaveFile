9295c1719572cd8027c00d792282e1f7200abf43
Lettever <61094520+Lettever@users.noreply.github.com>
2024-05-19 17:27:59 +0100

    Add files via upload

--- rorrSaveFile.py
+++ rorrSaveFile.py
@@ -4,12 +4,13 @@ import re
 class SaveFile:
     def __init__(self, steamId3):
         ls = set()
+        l = set()
         self.commands = {}
         with open('a.txt', 'r') as f:
             for x in f.readlines():
+                if x.startswith('#'):
+                    continue
                 x = x.strip()
-                if x == 'END':
-                    break
                 self.commands[x] = eval(f'self.{x}')
         file_path = r'C:\Program Files (x86)\Steam\userdata\\' + str(steamId3) + r'\1337520\remote\save.json'
         info = {}
@@ -20,10 +21,11 @@ class SaveFile:
                     name, tag = x[0]
                     ls.add(f'{name}')
                     ls.add(f'{name}_{tag}')
+                    l.add(tag)
                     if name not in info:
                         info[name] = {}
                     info[name][tag] = int(v);
-        for x in sorted(list(ls)):
+        for x in sorted(list(l)):
             print('#', x, sep='')
         valid_keys = [
             'wins_hard',
@@ -40,6 +42,7 @@ class SaveFile:
                     info[i][k] = 0
         self.info = info
 
+    """
     def print(self):
         valid_keys = [
             'wins_hard',
@@ -58,9 +61,77 @@ class SaveFile:
                 key = ' '.join([word.capitalize() for word in key.split('_')])
                 print('    ', key, ': ', x, sep='')
             print('}\n')
-
+    """
+    def test(self, name):
+        wins = self.info[name]['wins']
+        deaths = self.info[name]['deaths']
+        games_played = self.info[name]['games_played']
+        total_items = self.info[name]['total_items']
+        total_kills = self.info[name]['total_kills']
+        total_stages = self.info[name]['total_stages']
+        wins = self.info[name]['wins']
+        wins_hard = self.info[name]['wins_hard']
+        win_ratio = (wins - wins_hard) / games_played
+        win_ratio = round(win_ratio * 100, 2)
+        win_hard_ratio = wins_hard / games_played
+        win_hard_ratio = round(win_hard_ratio * 100, 2)
+        loss_ratio = deaths / games_played
+        loss_ratio = round(loss_ratio * 100, 2)
+        abandonment_ratio = (games_played - wins - deaths) / games_played
+        abandonment_ratio = round(abandonment_ratio * 100, 2)
+        total_picks = 0
+        for x in self.info.values():
+            total_picks += x['games_played']
+        pick_ratio = games_played / total_picks
+        pick_ratio = round(pick_ratio * 100, 2)
+        print('wins', wins)
+        print('deaths', deaths)
+        print('games_played', games_played)
+        print('total_items', total_items)
+        print('total_kills', total_kills)
+        print('total_stages', total_stages)
+        print('wins', wins)
+        print('wins_hard', wins_hard)
+        print('win_ratio', win_ratio)
+        print('win_hard_ratio', win_hard_ratio)
+        print('loss_ratio', loss_ratio)
+        print('abandonment_ratio', abandonment_ratio)
+        print('pick_ratio', pick_ratio)
     def enforcer(self):
-        print('enforcer function')
+        wins = self.info['enforcer']['wins']
+        deaths = self.info['enforcer']['deaths']
+        games_played = self.info['enforcer']['games_played']
+        total_items = self.info['enforcer']['total_items']
+        total_kills = self.info['enforcer']['total_kills']
+        total_stages = self.info['enforcer']['total_stages']
+        wins = self.info['enforcer']['wins']
+        wins_hard = self.info['enforcer']['wins_hard']
+        win_ratio = (wins - wins_hard) / games_played
+        win_ratio = round(win_ratio * 100, 2)
+        win_hard_ratio = wins_hard / games_played
+        win_hard_ratio = round(win_hard_ratio * 100, 2)
+        loss_ratio = deaths / games_played
+        loss_ratio = round(loss_ratio * 100, 2)
+        abandonment_ratio = (games_played - wins - deaths) / games_played
+        abandonment_ratio = round(abandonment_ratio * 100, 2)
+        total_picks = 0
+        for x in self.info.values():
+            total_picks += x['games_played']
+        pick_ratio = games_played / total_picks
+        pick_ratio = round(pick_ratio * 100, 2)
+        print('wins', wins)
+        print('deaths', deaths)
+        print('games_played', games_played)
+        print('total_items', total_items)
+        print('total_kills', total_kills)
+        print('total_stages', total_stages)
+        print('wins', wins)
+        print('wins_hard', wins_hard)
+        print('win_ratio', win_ratio)
+        print('win_hard_ratio', win_hard_ratio)
+        print('loss_ratio', loss_ratio)
+        print('abandonment_ratio', abandonment_ratio)
+        print('pick_ratio', pick_ratio)
     def huntress(self):
         print('huntress function')
     def chef(self):
@@ -129,16 +200,16 @@ def test(string):
     
 def main():
     data = SaveFile(1047710596)
-    data.print()
     print(test("abc_def_ghi"))
     while (line := input(">> ")):
-        _, fn = best_fit(line, data.commands)
+        name, fn = best_fit(line, data.commands)
         if line == '?':
             for x in data.commands:
                 print('  ', x)
         elif fn is None:
             print("What")
         else:
+            print(name)
             fn()
 
 if __name__ == '__main__':
